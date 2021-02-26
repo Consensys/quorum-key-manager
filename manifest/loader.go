83e56f768b8e010c1dd692c699f8b5e2fc793e7f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-02-26 17:41:06 +0100

    add manifest loader

---
+++ manifest/loader.go
@@ -0,0 +1,31 @@
+package manifest
+
+// Message wraps a manifest with information related to the Loader that loaded it
+type Message struct {
+	// Name of the loader that loaded the manifest
+	Loader string
+
+	// Manifest loaded
+	Manifest *Manifest
+
+	// Action to perform (e.g. create, update, delete...)
+	Action string
+
+	// Err while loading manifest
+	Err error
+}
+
+// Loader loads and broadcast manifests
+type Loader interface {
+	// Subscribe creates a subscription that will write all Manifest matching the
+	// given kinds to the given mnfsts channel
+
+	// If no kind is passed then all manifest are written
+	Subscribe(mnfsts chan<- []*Message) (Subscription, error)
+}
+
+// Subscription
+type Subscription interface {
+	Unsubscribe() error
+	Error() <-chan error
+}
