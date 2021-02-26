bba64e03b3265b5e83bd5f8faffec6a25d62057f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-02-26 18:02:42 +0100

    add docstring

--- app/app.go
+++ app/app.go
@@ -3,12 +3,18 @@ package app
 import (
 	"net"
 	"net/http"
+
+	"github.com/ConsenSysQuorum/quorum-key-manager/app/backend"
 )
 
+// App is the main Key Manager application object
 type App struct {
+	// listener accepting HTTP connection
 	listener net.Listener
 
+	// httpServer processing entrying HTTP request
 	httpServer *http.Server
 
+	// backend managing core backend components
 	backend backend.Backend
 }
