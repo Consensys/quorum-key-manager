1ba2ddd7a8570187a6e46f48aac80ba0b0596ce0
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-02-26 17:58:58 +0100

    clean docstring

--- app/api/api.go
+++ app/api/api.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ConsenSysQuorum/quorum-key-manager/app/backend"
 )
 
-// New creates the http.Handler processing all transactions
+// New creates the http.Handler processing all http requests
 func New(bcknd backend.Backend) http.Handler {
 	// TODO: to be implemented
 	return nil
