5bb581fbb9fe7a0b7580417a4d601f0c5210311e
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-02-28 23:12:22 +0100

    extend backend interface

--- backend/backend.go
+++ backend/backend.go
@@ -1,16 +1,24 @@
 package backend
 
 import (
+	"github.com/ConsenSysQuorum/quorum-key-manager/audit"
+	policymanager "github.com/ConsenSysQuorum/quorum-key-manager/auth/policy/manager"
 	"github.com/ConsenSysQuorum/quorum-key-manager/manifest"
-	"github.com/ConsenSysQuorum/quorum-key-manager/store/manager"
+	storemanager "github.com/ConsenSysQuorum/quorum-key-manager/store/manager"
 )
 
-// Backend synchronizes internal Key Manager components
-// and provide access to it
+// Backend holds internal Key Manager components and
+// makes it available for API components
 type Backend interface {
 	// StoreManager returns the Store Manager
-	StoreManager() manager.Manager
+	StoreManager() storemanager.Manager
 
 	// ManifestLoader returns the Manifest Loader
 	ManifestLoader() manifest.Loader
+
+	// PolicyManager returns the PolicyManager
+	PolicyManager() policymanager.Manager
+
+	// Auditor returns the PolicyManager
+	Auditor() audit.Auditor
 }
