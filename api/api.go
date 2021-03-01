36eff1f6953669eb93c164753c27ce5308a0662e
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-03-01 13:23:04 +0100

    add HTTP middleware

--- api/api.go
+++ api/api.go
@@ -13,10 +13,23 @@ import (
 
 // New creates the http.Handler processing all http requests
 func New(bcknd backend.Backend) http.Handler {
+	// Create HTTP Middleware
+	mid := NewHTTPMiddleware(bcknd)
+
+	// Create router
 	r := mux.NewRouter()
 	r.PathPrefix("/secrets").Handler(secretsapi.New(bcknd))
 	r.PathPrefix("/keys").Handler(keysapi.New(bcknd))
 	r.PathPrefix("/accounts").Handler(accountsapi.New(bcknd))
 	r.Path("/jsonrpc").Handler(jsonrpcapi.New(bcknd))
-	return r
+
+	// Return wrapped router
+	return mid(r)
+}
+
+func NewHTTPMiddleware(bcknd backend.Backend) func(http.Handler) http.Handler {
+	// TODO: implement the sequence of middlewares to apply before routing
+	return func(h http.Handler) http.Handler {
+		return h
+	}
 }
