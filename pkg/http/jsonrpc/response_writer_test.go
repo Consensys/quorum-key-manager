67a4635c80fc41f5bbe57cb2f09f86374e08ce6f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-03-15 10:31:32 +0100

    test response writer

---
+++ pkg/http/jsonrpc/response_writer_test.go
@@ -0,0 +1,86 @@
+package jsonrpc
+
+import (
+	"fmt"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestResponseWriterHeader(t *testing.T) {
+	rec := httptest.NewRecorder()
+	rw := NewResponseWriter(rec)
+
+	rw.Header().Add("test-key", "test-value")
+	assert.Equal(t, "test-value", rec.Header().Get("test-key"), "Header should have been set")
+}
+
+func TestResponseWriterWrite(t *testing.T) {
+	rec := httptest.NewRecorder()
+	rw := NewResponseWriter(rec)
+
+	n, err := rw.Write([]byte(`t`))
+	require.NoError(t, err, "Write should not error")
+	assert.Equal(t, n, 1, "Write should return correct value")
+
+	b, err := rec.Body.ReadByte()
+	require.NoError(t, err, "ReadByte should not error")
+	assert.Equal(t, b, []byte(`t`)[0], "Write should have wrote correctly")
+}
+
+func TestResponseWriterWriteMsg(t *testing.T) {
+	// WriteMsg with fields set
+	rec := httptest.NewRecorder()
+	rw := NewResponseWriter(rec)
+
+	msg := &ResponseMsg{
+		Version: "3.0",
+		ID:      "39",
+		Result:  true,
+	}
+	err := rw.WriteMsg(msg)
+	require.NoError(t, err, "WriteMsg should not error")
+
+	expectedBody := []byte(`{"jsonrpc":"3.0","id":"39","result":true,"error":null}`)
+	assert.Equal(t, expectedBody, rec.Body.Bytes()[:(rec.Body.Len()-1)], "WriteMsg should write correct body")
+
+	// WriteMsg with deafult values set
+	rec = httptest.NewRecorder()
+	rw = NewResponseWriter(rec)
+
+	msg = &ResponseMsg{
+		Result: true,
+	}
+	err = rw.WriteMsg(msg)
+	require.NoError(t, err, "WriteMsg should not error")
+
+	expectedBody = []byte(`{"jsonrpc":"2.0","id":null,"result":true,"error":null}`)
+	assert.Equal(t, expectedBody, rec.Body.Bytes()[:(rec.Body.Len()-1)], "WriteMsg should write correct body")
+}
+
+func TestResponseWriterWriteResult(t *testing.T) {
+	rec := httptest.NewRecorder()
+	rw := NewResponseWriter(rec)
+
+	rw.WithID(1234)
+	rw.WithVersion("1.0")
+
+	err := rw.WriteResult(false)
+	require.NoError(t, err, "WriteResult should not error")
+	expectedBody := []byte(`{"jsonrpc":"1.0","id":1234,"result":false,"error":null}`)
+	assert.Equal(t, expectedBody, rec.Body.Bytes()[:(rec.Body.Len()-1)], "WriteResult should write correct body")
+}
+
+func TestResponseWriterWriteError(t *testing.T) {
+	rec := httptest.NewRecorder()
+	rw := NewResponseWriter(rec)
+
+	rw.WithID("abcd")
+
+	err := rw.WriteError(fmt.Errorf("test error"))
+	require.NoError(t, err, "WriteError should not error")
+	expectedBody := []byte(`{"jsonrpc":"2.0","id":"abcd","result":null,"error":{"code":0,"message":"test error","data":null}}`)
+	assert.Equal(t, expectedBody, rec.Body.Bytes()[:(rec.Body.Len()-1)], "WriteError should write correct body")
+}
