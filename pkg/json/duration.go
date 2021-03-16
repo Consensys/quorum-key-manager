68e6aba3be5b7b3599be002a749edfca271e3131
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-03-16 21:30:21 +0100

    add jsonable Duration

---
+++ pkg/json/duration.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) (err error) {
+	if b[0] == '"' {
+		sd := string(b[1 : len(b)-1])
+		d.Duration, err = time.ParseDuration(sd)
+		return
+	}
+
+	var id int64
+	id, err = json.Number(string(b)).Int64()
+	d.Duration = time.Duration(id)
+
+	return
+}
+
+func (d Duration) MarshalJSON() (b []byte, err error) {
+	return []byte(fmt.Sprintf(`"%s"`, d.String())), nil
+}
