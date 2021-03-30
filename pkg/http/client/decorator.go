f5d81af47462026ff57aef3eb5ddf03505348e8c
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-03-30 09:52:19 +0200

    implement http Client

---
+++ pkg/http/client/decorator.go
@@ -0,0 +1,72 @@
+package httpclient
+
+import (
+	"net/http"
+
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/request"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/response"
+)
+
+// Decorator decorates a Client
+type Decorator func(Client) Client
+
+func WithPreparer(preparer request.Preparer) Decorator {
+	return func(c Client) Client {
+		return &preparedClient{
+			client:   c,
+			preparer: preparer,
+		}
+	}
+}
+
+type preparedClient struct {
+	client Client
+
+	preparer request.Preparer
+}
+
+func (c *preparedClient) Do(req *http.Request) (*http.Response, error) {
+	preparedReq, err := c.preparer.Prepare(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.client.Do(preparedReq)
+}
+
+func (c *preparedClient) CloseIdleConnections() {
+	c.client.CloseIdleConnections()
+}
+
+type modifiedClient struct {
+	client Client
+
+	modifier response.Modifier
+}
+
+func WithModifier(modifier response.Modifier) Decorator {
+	return func(c Client) Client {
+		return &modifiedClient{
+			client:   c,
+			modifier: modifier,
+		}
+	}
+}
+
+func (c *modifiedClient) Do(req *http.Request) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.modifier.Modify(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (c *modifiedClient) CloseIdleConnections() {
+	c.client.CloseIdleConnections()
+}
