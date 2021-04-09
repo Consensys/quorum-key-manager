b3311bc85210ed46a16dd6b79d95917daad8e87c
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-04-09 14:52:23 +0200

    implement node interface to downstreams

---
+++ src/infra/node/config.go
@@ -0,0 +1,73 @@
+package node
+
+import (
+	httpclient "github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/client"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/request"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/response"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/transport"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/json"
+)
+
+type ProxyConfig struct {
+	Request  *request.ProxyConfig  `json:"request,omitempty"`
+	Response *response.ProxyConfig `json:"response,omitempty"`
+}
+
+func (cfg *ProxyConfig) SetDefault() {
+	if cfg.Request == nil {
+		cfg.Request = new(request.ProxyConfig)
+	}
+
+	if cfg.Response == nil {
+		cfg.Response = new(response.ProxyConfig)
+	}
+}
+
+// DownstreamConfig
+type DownstreamConfig struct {
+	Addr          string            `json:"addr,omitempty"`
+	Transport     *transport.Config `json:"transport,omitempty"`
+	Proxy         *ProxyConfig      `json:"proxy,omitempty"`
+	ClientTimeout *json.Duration    `json:"timeout,omitempty"`
+}
+
+func (cfg *DownstreamConfig) SetDefault() {
+	defaultCfg := new(httpclient.Config)
+	defaultCfg.SetDefault()
+
+	if cfg.Transport == nil {
+		cfg.Transport = defaultCfg.Transport
+	}
+
+	if cfg.Proxy == nil {
+		cfg.Proxy = new(ProxyConfig)
+	}
+
+	if cfg.Proxy.Request == nil {
+		cfg.Proxy.Request = defaultCfg.Request
+	}
+
+	if cfg.Proxy.Request.Addr == "" {
+		cfg.Proxy.Request.Addr = cfg.Addr
+	}
+
+	if cfg.Proxy.Response == nil {
+		cfg.Proxy.Response = defaultCfg.Response
+	}
+
+	if cfg.ClientTimeout == nil {
+		cfg.ClientTimeout = defaultCfg.Timeout
+	}
+}
+
+// Config is the cfg format for an Hashicorp Vault secret store
+type Config struct {
+	RPC           *DownstreamConfig `json:"json-rpc,omitempty"`
+	PrivTxManager *DownstreamConfig `json:"tessera,omitempty"`
+}
+
+func (cfg *Config) SetDefault() {
+	if cfg.RPC == nil {
+		cfg.RPC.SetDefault()
+	}
+}
---
+++ src/infra/node/node.go
@@ -0,0 +1,189 @@
+package node
+
+import (
+	"net/http"
+
+	httpclient "github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/client"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/jsonrpc"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/proxy"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/request"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/response"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/transport"
+)
+
+//go:generate mockgen -source=node.go -destination=mock/node.go -package=mock
+
+// Node holds interface to connect to a downstream Quorum node including JSON-RPC server and private transaction manager
+type Node interface {
+	// ClientRPC returns client to downstream JSON-RPC
+	ClientRPC() jsonrpc.Client
+
+	// Proxy returns an JSON-RPC proxy to the downstream JSON-RPC node
+	ProxyRPC() jsonrpc.Handler
+
+	// ClientPrivTxManager returns client to downstrem private transaction manager
+	ClientPrivTxManager() httpclient.Client
+
+	// Proxy returns an HTTP proxy to the downstream Priv Tx Manager node
+	ProxyPrivTxManager() http.Handler
+
+	// Returns a session
+	Session(id string) Session
+}
+
+// Session holds client interface to a downstream node
+type Session interface {
+	// Node returns the node that generated this session
+	Node
+
+	// CallerRPC returns a caller to downstream JSON-RPC
+	CallerRPC() jsonrpc.Caller
+
+	// Close the session
+	Close()
+}
+
+// NewNode creates a new
+func NewNode(cfg *Config) (Node, error) {
+	cfg.SetDefault()
+	n := new(node)
+	var err error
+	n.rpc, err = newRPCDownstream(cfg.RPC)
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.PrivTxManager != nil {
+		n.privTMngr, err = newPrivTxMngrDownstream(cfg.PrivTxManager)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return n, nil
+}
+
+type node struct {
+	rpc       *rpcDownstream
+	privTMngr *privTxMngrDownstream
+}
+
+func (n *node) ClientRPC() jsonrpc.Client {
+	return n.rpc.client
+}
+
+func (n *node) ProxyRPC() jsonrpc.Handler {
+	return n.rpc.proxy
+}
+
+func (n *node) ClientPrivTxManager() httpclient.Client {
+	return n.privTMngr.http.client
+}
+
+func (n *node) ProxyPrivTxManager() http.Handler {
+	return n.privTMngr.http.proxy
+}
+
+func (n *node) Session(id string) Session {
+	return &session{
+		node:      n,
+		rpcCaller: jsonrpc.NewCaller(jsonrpc.WithID(id)(n.rpc.client)),
+	}
+}
+
+type session struct {
+	*node
+	rpcCaller jsonrpc.Caller
+}
+
+func (s *session) CallerRPC() jsonrpc.Caller {
+	return s.rpcCaller
+}
+
+func (s *session) Close() {
+	// TODO: to be implemented in particular maybe it makes sense to recycle sessions using a sync.Pool
+}
+
+type httpDownstream struct {
+	transport    http.RoundTripper
+	reqPreparer  request.Preparer
+	respModifier response.Modifier
+	client       httpclient.Client
+	proxy        http.Handler
+}
+
+func newhttpDownstream(cfg *DownstreamConfig) (*httpDownstream, error) {
+	cfg.SetDefault()
+
+	n := new(httpDownstream)
+	var err error
+	n.transport, err = transport.New(cfg.Transport)
+	if err != nil {
+		return nil, err
+	}
+
+	n.reqPreparer, err = request.Proxy(cfg.Proxy.Request)
+	if err != nil {
+		return nil, err
+	}
+
+	n.respModifier = response.Proxy(cfg.Proxy.Response)
+
+	n.client, err = httpclient.New(&httpclient.Config{Timeout: cfg.ClientTimeout}, n.transport, n.reqPreparer, n.respModifier)
+	if err != nil {
+		return nil, err
+	}
+
+	n.proxy, err = proxy.New(nil, n.transport, n.reqPreparer, n.respModifier, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return n, nil
+}
+
+type rpcDownstream struct {
+	http   *httpDownstream
+	proxy  jsonrpc.Handler
+	client jsonrpc.Client
+}
+
+func newRPCDownstream(cfg *DownstreamConfig) (*rpcDownstream, error) {
+	cfg.SetDefault()
+
+	n := new(rpcDownstream)
+	var err error
+	n.http, err = newhttpDownstream(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	n.client = jsonrpc.NewClient(n.http.client)
+
+	// Overide HTTP Proy
+	n.http.proxy, err = proxy.New(nil, n.http.transport, n.http.reqPreparer, n.http.respModifier, jsonrpc.HandleProxyRoundTripError, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	n.proxy = jsonrpc.FromHTTPHandler(n.http.proxy)
+
+	return n, nil
+}
+
+type privTxMngrDownstream struct {
+	http *httpDownstream
+}
+
+func newPrivTxMngrDownstream(cfg *DownstreamConfig) (*privTxMngrDownstream, error) {
+	cfg.SetDefault()
+
+	n := new(privTxMngrDownstream)
+	var err error
+	n.http, err = newhttpDownstream(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return n, nil
+}
