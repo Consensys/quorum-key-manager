f8268060b372ea7f366f314df610ea1882141e7a
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-04-12 12:47:31 +0200

    refactor Body preparer

--- pkg/http/request/preparer_body.go
+++ pkg/http/request/preparer_body.go
@@ -10,18 +10,20 @@ import (
 // Body ensures that GetBody method is set on request
 func Body() Preparer {
 	return PrepareFunc(func(req *http.Request) (*http.Request, error) {
-		if req.GetBody == nil {
-			// See More https://github.com/golang/net/blob/master/http2/transport.go#L554
-			if req.Body != nil {
-				body, _ := ioutil.ReadAll(req.Body)
-				req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-				req.GetBody = func() (io.ReadCloser, error) {
-					return ioutil.NopCloser(bytes.NewBuffer(body)), nil
-				}
-			} else {
-				req.GetBody = func() (io.ReadCloser, error) {
-					return nil, nil
-				}
+		if req.GetBody != nil {
+			return req, nil
+		}
+
+		// See More https://github.com/golang/net/blob/master/http2/transport.go#L554
+		if req.Body != nil {
+			body, _ := ioutil.ReadAll(req.Body)
+			req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+			req.GetBody = func() (io.ReadCloser, error) {
+				return ioutil.NopCloser(bytes.NewBuffer(body)), nil
+			}
+		} else {
+			req.GetBody = func() (io.ReadCloser, error) {
+				return nil, nil
 			}
 		}
 
