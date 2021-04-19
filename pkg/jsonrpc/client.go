6e356de2a4097b9f04500c654cbe0d97d80941c5
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-04-19 19:22:10 +0200

    move caller in a dedicated file

--- pkg/jsonrpc/client.go
+++ pkg/jsonrpc/client.go
@@ -1,7 +1,6 @@
 package jsonrpc
 
 import (
-	"context"
 	"fmt"
 	"reflect"
 	"sync/atomic"
@@ -160,39 +159,4 @@ func (c *versionClient) Do(req *Request) (*Response, error) {
 
 func (c *versionClient) CloseIdleConnections() {
 	c.client.CloseIdleConnections()
-}
-
-// Caller is an interface for a JSON-RPC caller
-type Caller interface {
-	Call(ctx context.Context, method string, params interface{}) (*Response, error)
-}
-
-type caller struct {
-	client Client
-	req    *Request
-}
-
-func NewCaller(c Client, req *Request) Caller {
-	return &caller{
-		client: c,
-		req:    req,
-	}
-}
-
-// Call sends a JSON-RPC request over underlying http.Transport
-
-// Returns an http.Response which body as already been consumed in the jsonrpc.ResponseMsg
-
-// It returns an error in following scenarios
-// - underlying transport failed to roundtrip
-// - response status code is not 2XX
-// - response body is an invalid JSON-RPC response
-// - JSON-RPC response is failed (in which case it returns the jsonrpc.ErrorMsg)
-func (c *caller) Call(ctx context.Context, method string, params interface{}) (*Response, error) {
-	req := RequestFromContext(ctx)
-	if req == nil {
-		req = c.req
-	}
-
-	return c.client.Do(req.Clone(ctx).WithMethod(method).WithParams(params))
 }
