2798e9652e80976d3e584890a38028bf440db45d
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-05-04 01:42:06 +0200

    feat@pkg/http(header): add utilities to manipulate http headers

---
+++ pkg/http/header/utils.go
@@ -0,0 +1,23 @@
+package header
+
+import (
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/http/httpguts"
+)
+
+func Copy(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func UpgradeType(h http.Header) string {
+	if !httpguts.HeaderValuesContainsToken(h["Connection"], "Upgrade") {
+		return ""
+	}
+	return strings.ToLower(h.Get("Upgrade"))
+}
