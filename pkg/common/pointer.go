75e6f4d4078fba059f55d779bd60d3720b6bcd89
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-05-10 19:30:28 +0200

    refactor@pkg(common): clean pointer transform declaration

--- pkg/common/pointer.go
+++ pkg/common/pointer.go
@@ -2,6 +2,22 @@ package common
 
 import "reflect"
 
+func Tomapstrptr(m map[string]string) map[string]*string {
+	nm := make(map[string]*string)
+	for k, v := range m {
+		nm[k] = &(&struct{ x string }{v}).x
+	}
+	return nm
+}
+
+func Tomapstr(m map[string]*string) map[string]string {
+	nm := make(map[string]string)
+	for k, v := range m {
+		nm[k] = *v
+	}
+	return nm
+}
+
 func ToPtr(v interface{}) interface{} {
 	p := reflect.New(reflect.TypeOf(v))
 	p.Elem().Set(reflect.ValueOf(v))
