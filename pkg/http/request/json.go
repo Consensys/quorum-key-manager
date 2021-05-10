0660828354138e7610060a4bc2590919710e6cf6
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-05-11 00:57:55 +0200

    fix@pkg/http(request): update WriteJSON

--- pkg/http/request/json.go
+++ pkg/http/request/json.go
@@ -3,6 +3,7 @@ package request
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -15,15 +16,21 @@ func WriteJSON(req *http.Request, msg interface{}) error {
 	req.Header.Set("Content-Type", "application/json")
 
 	// Write message into buffer
-	buf := new(bytes.Buffer)
-	err := json.NewEncoder(buf).Encode(msg)
+	b, err := json.Marshal(msg)
 	if err != nil {
 		return err
 	}
 
+	body := bytes.NewBuffer(b)
+
 	// Set body
-	req.Body = ioutil.NopCloser(buf)
-	req.ContentLength = int64(buf.Len())
+	req.Body = ioutil.NopCloser(body)
+	req.ContentLength = int64(body.Len())
+	buf := body.Bytes()
+	req.GetBody = func() (io.ReadCloser, error) {
+		r := bytes.NewReader(buf)
+		return ioutil.NopCloser(r), nil
+	}
 
 	return nil
 }
