0f714705f6b4e6717960e45b0d1fd20ab0118a68
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2021-05-28 11:13:44 +0200

    refactor@src(api): move StripPrefix utility in pkg/http/middleware

---
+++ pkg/http/middleware/strip_prefix.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// Modified version of http.StripPrefix() to append a tail backslash in case prefix exact match with URL.path
+func StripPrefix(prefix string) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if p := strings.TrimPrefix(r.URL.Path, prefix); len(p) < len(r.URL.Path) {
+				r2 := new(http.Request)
+				*r2 = *r
+				r2.URL = new(url.URL)
+				*r2.URL = *r.URL
+				if p == "" {
+					p = "/"
+				}
+
+				r2.URL.Path = p
+				h.ServeHTTP(w, r2)
+			} else {
+				http.NotFound(w, r)
+			}
+		})
+	}
+}
--- src/api/middleware/strip-prefix.go
+++ src/api/middleware/strip-prefix.go
@@ -2,36 +2,25 @@ package middleware
 
 import (
 	"net/http"
-	"net/url"
 	"strings"
 
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/http/middleware"
 	"github.com/ConsenSysQuorum/quorum-key-manager/src/api/handlers"
 )
 
 const StoreURLPlaceholder = "{.+}"
 
 // Modified version of http.StripPrefix() to append a tail backslash in case prefix exact match with URL.path
-func StripPrefix(prefix string, h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		urlPrefix := prefix
-		if strings.Contains(prefix, StoreURLPlaceholder) {
-			storeID := r.Context().Value(handlers.StoreContextID)
-			urlPrefix = strings.Replace(urlPrefix, StoreURLPlaceholder, storeID.(string), 1)
-		}
-
-		if p := strings.TrimPrefix(r.URL.Path, urlPrefix); len(p) < len(r.URL.Path) {
-			r2 := new(http.Request)
-			*r2 = *r
-			r2.URL = new(url.URL)
-			*r2.URL = *r.URL
-			if p == "" {
-				p = "/"
+func StripPrefix(prefix string) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			urlPrefix := prefix
+			if strings.Contains(prefix, StoreURLPlaceholder) {
+				storeID := r.Context().Value(handlers.StoreContextID)
+				urlPrefix = strings.Replace(urlPrefix, StoreURLPlaceholder, storeID.(string), 1)
 			}
 
-			r2.URL.Path = p
-			h.ServeHTTP(w, r2)
-		} else {
-			http.NotFound(w, r)
-		}
-	})
+			middleware.StripPrefix(urlPrefix)(h)
+		})
+	}
 }
