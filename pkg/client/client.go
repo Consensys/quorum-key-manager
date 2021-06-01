cce620e5ac37413611a07e3d99eb383590e5d664
Dario Anongba Varela <dario.varela@consensys.net>
2021-06-01 16:25:35 +0200

    before e2e

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -18,7 +18,7 @@ type SecretsClient interface {
 type KeysClient interface {
 	CreateKey(ctx context.Context, storeName string, request *types.CreateKeyRequest) (*types.KeyResponse, error)
 	ImportKey(ctx context.Context, storeName string, request *types.ImportKeyRequest) (*types.KeyResponse, error)
-	Sign(ctx context.Context, storeName, id string, request *types.SignBase64PayloadRequest) (string, error)
+	SignKey(ctx context.Context, storeName, id string, request *types.SignBase64PayloadRequest) (string, error)
 	GetKey(ctx context.Context, storeName, id string) (*types.KeyResponse, error)
 	ListKeys(ctx context.Context, storeName string) ([]string, error)
 	DestroyKey(ctx context.Context, storeName, id string) error
@@ -26,6 +26,21 @@ type KeysClient interface {
 
 type Eth1Client interface {
 	CreateEth1Account(ctx context.Context, storeName string, request *types.CreateEth1AccountRequest) (*types.Eth1AccountResponse, error)
+	ImportEth1Account(ctx context.Context, storeName string, request *types.ImportEth1AccountRequest) (*types.Eth1AccountResponse, error)
+	UpdateEth1Account(ctx context.Context, storeName string, request *types.UpdateEth1AccountRequest) (*types.Eth1AccountResponse, error)
+	SignEth1(ctx context.Context, storeName string, request *types.SignHexPayloadRequest) (string, error)
+	SignTypedData(ctx context.Context, storeName string, request *types.SignTypedDataRequest) (string, error)
+	SignTransaction(ctx context.Context, storeName string, request *types.SignETHTransactionRequest) (string, error)
+	SignQuorumPrivateTransaction(ctx context.Context, storeName string, request *types.SignQuorumPrivateTransactionRequest) (string, error)
+	SignEEATransaction(ctx context.Context, storeName string, request *types.SignEEATransactionRequest) (string, error)
+	GetEth1Account(ctx context.Context, storeName, account string) (*types.Eth1AccountResponse, error)
+	ListEth1Account(ctx context.Context, storeName string) ([]string, error)
+	DeleteEth1Account(ctx context.Context, storeName, account string, ) error
+	DestroyEth1Account(ctx context.Context, storeName, account string) error
+	RestoreEth1Account(ctx context.Context, storeName, account string) error
+	ECRecover(ctx context.Context, storeName string, request *types.ECRecoverRequest) (string, error)
+	VerifySignature(ctx context.Context, storeName string, request *types.VerifyEth1SignatureRequest) error
+	VerifyTypedDataSignature(ctx context.Context, storeName string, request *types.VerifyTypedDataRequest) error
 }
 
 type JSONRPC interface {
---
+++ tests/e2e/eth1_test.go
@@ -0,0 +1,90 @@
+// +build e2e
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"github.com/ConsenSysQuorum/quorum-key-manager/src/services/stores/api/types/testutils"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/client"
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/common"
+	"github.com/ConsenSysQuorum/quorum-key-manager/tests"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+// const ecdsaPrivKeyHex = ""
+
+type eth1TestSuite struct {
+	suite.Suite
+	err              error
+	ctx              context.Context
+	keyManagerClient *client.HTTPClient
+	cfg              *tests.Config
+}
+
+func (s *eth1TestSuite) SetupSuite() {
+	if s.err != nil {
+		s.T().Error(s.err)
+	}
+
+	s.keyManagerClient = client.NewHTTPClient(&http.Client{}, &client.Config{
+		URL: s.cfg.KeyManagerURL,
+	})
+}
+
+func (s *eth1TestSuite) TearDownSuite() {
+	if s.err != nil {
+		s.T().Error(s.err)
+	}
+}
+
+func TestKeyManagerEth1(t *testing.T) {
+	s := new(eth1TestSuite)
+
+	s.ctx = context.Background()
+	sig := common.NewSignalListener(func(signal os.Signal) {
+		s.err = fmt.Errorf("interrupt signal was caught")
+		t.FailNow()
+	})
+	defer sig.Close()
+
+	s.cfg, s.err = tests.NewConfig()
+	suite.Run(t, s)
+}
+
+func (s *eth1TestSuite) TestCreate() {
+	s.Run("should create a new account successfully", func() {
+		request := testutils.FakeCreateEth1AccountRequest()
+
+		acc, err := s.keyManagerClient.CreateEth1Account(s.ctx, s.cfg.HashicorpKeyStore, request)
+		require.NoError(s.T(), err)
+
+		assert.NotEmpty(s.T(), acc.Address)
+		assert.NotEmpty(s.T(), acc.PublicKey)
+		assert.NotEmpty(s.T(), acc.CompressedPublicKey)
+		assert.Equal(s.T(), request.ID, acc.ID)
+		assert.Equal(s.T(), request.Tags, acc.Tags)
+		assert.False(s.T(), acc.Disabled)
+		assert.NotEmpty(s.T(), acc.CreatedAt)
+		assert.NotEmpty(s.T(), acc.UpdatedAt)
+		assert.True(s.T(), acc.ExpireAt.IsZero())
+		assert.True(s.T(), acc.DeletedAt.IsZero())
+		assert.True(s.T(), acc.DestroyedAt.IsZero())
+	})
+
+	s.Run("should parse errors successfully", func() {
+		request := testutils.FakeCreateEth1AccountRequest()
+
+		key, err := s.keyManagerClient.CreateEth1Account(s.ctx, "inexistentStoreName", request)
+		require.Nil(s.T(), key)
+
+		httpError := err.(*client.ResponseError)
+		assert.Equal(s.T(), 404, httpError.StatusCode)
+	})
+}
