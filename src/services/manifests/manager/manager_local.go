19c793d3b4df5a0f6c0f8e530191f56afcb0b7b2
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2021-06-01 16:17:55 +0200

    Update Manifest loading (#124)
    * implement tests for manifests local manager
    
    * update manifests loader to allow late subscriptions

--- src/services/manifests/manager/manager_local.go
+++ src/services/manifests/manager/manager_local.go
@@ -1,4 +1,4 @@
-package manifest
+package manager
 
 import (
 	"context"
@@ -18,17 +18,22 @@ type Config struct {
 }
 
 type LocalManager struct {
-	path          string
-	isDir         bool
-	subscriptions []*subscription
+	path  string
+	isDir bool
+
+	msgs []Message
+
+	loaded chan struct{}
+	err    error
 }
 
 func NewLocalManager(cfg *Config) (*LocalManager, error) {
 	fs, err := os.Stat(cfg.Path)
 	if err == nil {
 		return &LocalManager{
-			path:  cfg.Path,
-			isDir: fs.IsDir(),
+			path:   cfg.Path,
+			loaded: make(chan struct{}),
+			isDir:  fs.IsDir(),
 		}, nil
 	}
 
@@ -43,20 +48,43 @@ type subscription struct {
 	kinds    map[manifest.Kind]struct{}
 	messages chan<- []Message
 	errors   chan error
+	stop     chan struct{}
+	done     chan struct{}
 }
 
 func (sub *subscription) Unsubscribe() error {
+	close(sub.stop)
+	<-sub.done
 	close(sub.errors)
 	return nil
 }
 
 func (sub *subscription) Error() <-chan error { return sub.errors }
 
+func (sub *subscription) inbox(msgs []Message) {
+	var submsgs []Message
+	for _, msg := range msgs {
+		if sub.kinds == nil {
+			submsgs = append(submsgs, msg)
+			continue
+		}
+
+		if _, ok := sub.kinds[msg.Manifest.Kind]; ok {
+			submsgs = append(submsgs, msg)
+		}
+	}
+
+	sub.messages <- submsgs
+}
+
 func (ll *LocalManager) Subscribe(kinds []manifest.Kind, messages chan<- []Message) (Subscription, error) {
 	sub := &subscription{
 		messages: messages,
-		errors:   make(chan error),
+		errors:   make(chan error, 1),
+		stop:     make(chan struct{}),
+		done:     make(chan struct{}),
 	}
+
 	if kinds != nil {
 		sub.kinds = make(map[manifest.Kind]struct{})
 		for _, kind := range kinds {
@@ -64,15 +92,28 @@ func (ll *LocalManager) Subscribe(kinds []manifest.Kind, messages chan<- []Messa
 		}
 	}
 
-	ll.subscriptions = append(ll.subscriptions, sub)
+	go ll.processSub(sub)
 
 	return sub, nil
 }
 
-func (ll *LocalManager) Start(context.Context) error {
-	msgs := []Message{}
+func (ll *LocalManager) processSub(sub *subscription) {
+	defer close(sub.done)
+
+	select {
+	case <-ll.loaded:
+		if ll.err != nil {
+			sub.errors <- ll.err
+		} else {
+			sub.inbox(ll.msgs)
+		}
+	case <-sub.stop:
+	}
+}
+
+func (ll *LocalManager) load() error {
 	if ll.isDir {
-		err := filepath.Walk(ll.path, func(fp string, info os.FileInfo, err error) error {
+		return filepath.Walk(ll.path, func(fp string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
@@ -82,37 +123,22 @@ func (ll *LocalManager) Start(context.Context) error {
 			}
 
 			if filepath.Ext(fp) == ".yml" || filepath.Ext(fp) == ".yaml" {
-				msgs = append(msgs, ll.buildMessages(fp)...)
+				ll.msgs = append(ll.msgs, ll.buildMessages(fp)...)
 			}
 
 			return nil
 		})
-
-		if err != nil {
-			return err
-		}
-	} else {
-		msgs = append(msgs, ll.buildMessages(ll.path)...)
 	}
 
-	for _, sub := range ll.subscriptions {
-		go func(sub *subscription) {
-			var submsgs []Message
-			for _, msg := range msgs {
-				if sub.kinds == nil {
-					submsgs = append(submsgs, msg)
-					continue
-				}
-
-				if _, ok := sub.kinds[msg.Manifest.Kind]; ok {
-					submsgs = append(submsgs, msg)
-				}
-			}
-			sub.messages <- submsgs
-		}(sub)
-	}
+	ll.msgs = append(ll.msgs, ll.buildMessages(ll.path)...)
 
 	return nil
+}
+
+func (ll *LocalManager) Start(context.Context) error {
+	defer close(ll.loaded)
+	ll.err = ll.load()
+	return ll.err
 }
 
 func (ll *LocalManager) buildMessages(fp string) []Message {
@@ -150,10 +176,10 @@ func (ll *LocalManager) buildMessages(fp string) []Message {
 	return []Message{{
 		Loader: ManagerID,
 		Action: CreateAction,
-		Err:    errors.InvalidFormatError("cannot read manifest file %s", fp),
+		Err:    err,
 	}}
 }
 
 func (ll *LocalManager) Stop(context.Context) error { return nil }
-func (ll *LocalManager) Error() error               { return nil }
+func (ll *LocalManager) Error() error               { return ll.err }
 func (ll *LocalManager) Close() error               { return nil }
