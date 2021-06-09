8ab1a5aa4d5e46cb769d22b6c75d12540e6ad884
ggarri <ggarri@gmail.com>
2021-06-09 19:25:00 +0200

    #52 adding health endpoints e2e

--- tests/config.go
+++ tests/config.go
@@ -13,6 +13,7 @@ const envVar = "TEST_DATA"
 type Config struct {
 	AkvClient            *akvClient `json:"akv_client"`
 	KeyManagerURL        string     `json:"key_manager_url"`
+	HealthKeyManagerURL  string     `json:"health_key_manager_url"`
 	HashicorpSecretStore string     `json:"hashicorp_secret_store"`
 	HashicorpKeyStore    string     `json:"hashicorp_key_store"`
 	Eth1Store            string     `json:"eth1_store"`
---
+++ tests/e2e/healthz_test.go
@@ -0,0 +1,87 @@
+// +build e2e
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/ConsenSysQuorum/quorum-key-manager/pkg/common"
+	"github.com/ConsenSysQuorum/quorum-key-manager/src/stores/api/types"
+	"github.com/ConsenSysQuorum/quorum-key-manager/tests"
+	"github.com/stretchr/testify/suite"
+)
+
+type healthzTestSuite struct {
+	suite.Suite
+	err          error
+	ctx          context.Context
+	healthClient *http.Client
+	cfg          *tests.Config
+	mainAccount  *types.Eth1AccountResponse
+}
+
+func (s *healthzTestSuite) SetupSuite() {
+	if s.err != nil {
+		s.T().Error(s.err)
+	}
+
+	s.healthClient = &http.Client{}
+}
+
+func (s *healthzTestSuite) TearDownSuite() {
+	if s.err != nil {
+		s.T().Error(s.err)
+	}
+}
+
+func TestHealthz(t *testing.T) {
+	s := new(healthzTestSuite)
+
+	s.ctx = context.Background()
+	sig := common.NewSignalListener(func(signal os.Signal) {
+		s.err = fmt.Errorf("interrupt signal was caught")
+		t.FailNow()
+	})
+	defer sig.Close()
+
+	s.cfg, s.err = tests.NewConfig()
+	suite.Run(t, s)
+}
+
+func (s *healthzTestSuite) TestLiveness() {
+	s.Run("should validate liveness endpoint", func() {
+		
+	})
+}
+
+func (s *healthzTestSuite) TestReadiness() {
+	s.Run("should validate readiness endpoint", func() {
+		
+	})
+}
+
+func IsLive(ctx context.Context, client *http.Client, healthURL string) (bool, error) {
+	req, _ := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/live", healthURL), nil)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	return res.StatusCode == http.StatusOK, nil
+}
+
+func IsReady(ctx context.Context, client *http.Client, healthURL string) (bool, error) {
+	req, _ := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/ready", healthURL), nil)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	return res.StatusCode == http.StatusOK, nil
+}
