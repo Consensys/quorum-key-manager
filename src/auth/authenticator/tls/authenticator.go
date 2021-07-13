d5e868fbedce4b7819e8d068e9718a8f59d79f97
6l20 <sylvain.decourval@gmail.com>
2021-07-13 16:17:18 +0200

    Start TLS auth

--- src/auth/authenticator/tls/authenticator.go
+++ src/auth/authenticator/tls/authenticator.go
@@ -2,10 +2,9 @@ package tls
 
 import (
 	"crypto/x509"
-	"log"
 	"net/http"
 
-	"github.com/consensys/quorum-key-manager/pkg/errors"
+	"github.com/consensys/quorum-key-manager/src/auth/authenticator"
 	"github.com/consensys/quorum-key-manager/src/auth/types"
 )
 
@@ -14,6 +13,11 @@ const AuthMode = "Tls"
 type Authenticator struct {
 	Certificates []*x509.Certificate
 }
+
+var _ authenticator.Authenticator = Authenticator{}
+
+
+
 
 func NewAuthenticator(cfg *Config) (*Authenticator, error) {
 	if len(cfg.Certificates) == 0 {
@@ -25,29 +29,20 @@ func NewAuthenticator(cfg *Config) (*Authenticator, error) {
 	return auth, nil
 }
 
-// Authenticate checks certs and retrieve user Info
-// CN field -> Username
-// Organization -> Groups
-func (authenticator Authenticator) Authenticate(req *http.Request) (*types.UserInfo, error) {
+func (a Authenticator) Authenticate(req *http.Request) (*types.UserInfo, error) {
 	// extract Certificate info from request if any
-	if len(req.TLS.PeerCertificates) == 0 {
-		return nil, nil
-	}
-	// first array element is the leaf
-	clientCert := *req.TLS.PeerCertificates[0]
-	// check this cert matches authenticator provided one
-	// using strict comparison
-	for _, authCert := range authenticator.Certificates {
-		log.Printf("client cert %v\n, auth cert %v\n", clientCert.Subject, authCert.Subject)
-		if clientCert.Equal(authCert) {
-			return &types.UserInfo{
-				Username: clientCert.Subject.CommonName,
-				Groups:   clientCert.Subject.Organization,
-				AuthMode: AuthMode,
-			}, nil
+	if len(req.TLS.PeerCertificates) > 0 {
+		// As mentioned in doc first array element is the leaf
+		cert := req.TLS.PeerCertificates[0]
+		//TODO check this cert matches a provided one
+		return &types.UserInfo{
+			Username: cert.Subject.CommonName,
+			Groups:   cert.Subject.Organization,
+			AuthMode: AuthMode,
+		}, nil
 
-		}
 	}
 
-	return nil, errors.UnauthorizedError("no matching cert found")
+	return nil, nil
 }
+
