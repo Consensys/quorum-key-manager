c46d70f3995fdd43648d25e3fc8f026b01b3a8b2
6l20 <sylvain.decourval@gmail.com>
2021-07-13 16:17:18 +0200

    Start TLS auth

---
+++ src/auth/authenticator/tls/config.go
@@ -0,0 +1,25 @@
+package tls
+
+import (
+	"crypto/x509"
+)
+
+type Config struct {
+	Certificates []*x509.Certificate
+	Claims       *ClaimsConfig
+}
+
+type ClaimsConfig struct {
+	Username string
+	Group    string
+}
+
+func NewConfig(usernameClaim, groupClaims string, certs ...*x509.Certificate) *Config {
+	return &Config{
+		Certificates: certs,
+		Claims: &ClaimsConfig{
+			Username: usernameClaim,
+			Group:    groupClaims,
+		},
+	}
+}
