8723e6aea0b0d1b78b227223c2a1ca6d0054fc30
6l20 <sylvain.decourval@gmail.com>
2021-07-15 12:44:34 +0200

    Add U tests

--- src/auth/authenticator/tls/authenticator_test.go
+++ src/auth/authenticator/tls/authenticator_test.go
@@ -66,7 +66,7 @@ func TestAuthenticatorDifferentCert(t *testing.T) {
 		assert.Nil(t, userInfo)
 	})
 
-	t.Run("should return anonymous when client cert is missing", func(t *testing.T) {
+	t.Run("should reject when client cert is missing", func(t *testing.T) {
 
 		reqEve := httptest.NewRequest("GET", "https://test.url", nil)
 		reqEve.TLS = &tls.ConnectionState{}
@@ -85,7 +85,7 @@ func TestNilAuthenticator(t *testing.T) {
 		Certificates: []*x509.Certificate{},
 	})
 
-	t.Run("should not instantiate when no cert provided", func(t *testing.T) {
+	t.Run("should not instanciate when no cert provided", func(t *testing.T) {
 		assert.Nil(t, auth)
 	})
 
