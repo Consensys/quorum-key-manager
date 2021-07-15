92e396640e50ed4427b2a046e17aff980c5e88e4
6l20 <sylvain.decourval@gmail.com>
2021-07-15 08:25:51 +0200

    Rebase onto main

--- src/auth/config.go
+++ src/auth/config.go
@@ -3,9 +3,11 @@ package auth
 import (
 	apikey "github.com/consensys/quorum-key-manager/src/auth/authenticator/api-key"
 	"github.com/consensys/quorum-key-manager/src/auth/authenticator/oidc"
+	"github.com/consensys/quorum-key-manager/src/auth/authenticator/tls"
 )
 
 type Config struct {
-	OIDC   *oidc.Config
+	OIDC *oidc.Config
+	TlsCfg *tls.Config
 	APIKEY *apikey.Config
 }
