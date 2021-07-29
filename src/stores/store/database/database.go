614e0549052a1dc423e7fe1cc892896c1ad36ad0
Dario Varela <dario.varela@consensys.net>
2021-07-29 15:02:05 +0200

    models2

--- src/stores/store/database/database.go
+++ src/stores/store/database/database.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"context"
-	models2 "github.com/consensys/quorum-key-manager/src/stores/store/models"
+	"github.com/consensys/quorum-key-manager/src/stores/store/models"
 )
 
 //go:generate mockgen -source=database.go -destination=mock/database.go -package=mock
@@ -14,25 +14,25 @@ type Database interface {
 }
 
 type ETH1Accounts interface {
-	Get(ctx context.Context, addr string) (*models2.ETH1Account, error)
-	GetDeleted(ctx context.Context, addr string) (*models2.ETH1Account, error)
-	GetAll(ctx context.Context) ([]*models2.ETH1Account, error)
-	GetAllDeleted(ctx context.Context) ([]*models2.ETH1Account, error)
-	Add(ctx context.Context, account *models2.ETH1Account) error
-	Update(ctx context.Context, account *models2.ETH1Account) error
+	Get(ctx context.Context, addr string) (*models.ETH1Account, error)
+	GetDeleted(ctx context.Context, addr string) (*models.ETH1Account, error)
+	GetAll(ctx context.Context) ([]*models.ETH1Account, error)
+	GetAllDeleted(ctx context.Context) ([]*models.ETH1Account, error)
+	Add(ctx context.Context, account *models.ETH1Account) error
+	Update(ctx context.Context, account *models.ETH1Account) error
 	Delete(ctx context.Context, addr string) error
-	Restore(ctx context.Context, account *models2.ETH1Account) error
+	Restore(ctx context.Context, account *models.ETH1Account) error
 	Purge(ctx context.Context, addr string) error
 }
 
 type Keys interface {
-	Get(ctx context.Context, id string) (*models2.Key, error)
-	GetDeleted(ctx context.Context, id string) (*models2.Key, error)
-	GetAll(ctx context.Context) ([]*models2.Key, error)
-	GetAllDeleted(ctx context.Context) ([]*models2.Key, error)
-	Add(ctx context.Context, key *models2.Key) error
-	Update(ctx context.Context, key *models2.Key) error
+	Get(ctx context.Context, id string) (*models.Key, error)
+	GetDeleted(ctx context.Context, id string) (*models.Key, error)
+	GetAll(ctx context.Context) ([]*models.Key, error)
+	GetAllDeleted(ctx context.Context) ([]*models.Key, error)
+	Add(ctx context.Context, key *models.Key) error
+	Update(ctx context.Context, key *models.Key) error
 	Delete(ctx context.Context, id string) error
-	Restore(ctx context.Context, key *models2.Key) error
+	Restore(ctx context.Context, key *models.Key) error
 	Purge(ctx context.Context, id string) error
 }
