3196a69a00511166e69778c2f47e6d6a5c4fb9c3
6l20 <sylvain.decourval@gmail.com>
2021-08-03 10:38:45 +0200

    Removed auth certs

--- src/auth/authenticator/tls/config.go
+++ src/auth/authenticator/tls/config.go
@@ -5,11 +5,8 @@ import (
 )
 
 type Config struct {
-	Certificates []*x509.Certificate
 }
 
 func NewConfig(certs ...*x509.Certificate) *Config {
-	return &Config{
-		Certificates: certs,
-	}
+	return &Config{}
 }
