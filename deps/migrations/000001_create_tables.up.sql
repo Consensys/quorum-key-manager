4b4839c42d056371038075825c2678e6c4dd6e7e
Julien Marchand <julien.marchand4@gmail.com>
2021-09-22 17:38:00 +0200

    feat(migrations): split migrations for alias table creation

--- deps/migrations/000001_create_tables.up.sql
+++ deps/migrations/000001_create_tables.up.sql
@@ -44,12 +44,4 @@ CREATE TABLE IF NOT EXISTS eth_accounts (
     UNIQUE(address, store_id)
 );
 
-CREATE TABLE IF NOT EXISTS aliases (
-    pk SERIAL PRIMARY KEY,
-    key TEXT NOT NULL,
-    registry_name TEXT NOT NULL,
-    value TEXT NOT NULL,
-    UNIQUE(key, registry_name)
-);
-
 COMMIT;
