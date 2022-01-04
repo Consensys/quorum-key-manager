8a0620dd65dea2fb429fa771bb6a6ab571a285a2
ggarri <ggarri@gmail.com>
2022-01-04 11:18:52 +0100

    fix coverage script issue

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,10 +4,12 @@
 set -Eeu
 
 # Ignore generated & testutils files
-cat $1 | grep -Fv -e "/tests" -e "/mock" > $1
+cat $1 | grep -Fv -e "/tests" -e "/mock" > "$1.tmp"
 
 # Print total coverage
-go tool cover -func=$1 | grep total:
+go tool cover -func="$1.tmp" | grep total:
 
 # Generate coverage report in html format
-go tool cover -html=$1 -o $2
+go tool cover -html="$1.tmp" -o $2
+
+cat "$1.tmp" > $1
