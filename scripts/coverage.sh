926ecab2f901cbe8433db6c1f02ec718952befc6
ggarri <ggarri@gmail.com>
2022-01-04 11:09:03 +0100

    uploading out files instead of htmls

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,11 +4,11 @@
 set -Eeu
 
 # Ignore generated & testutils files
-cat $1 | grep -Fv -e "/deps" -e "/tests" -e "/public" > build/coverage/cover.out
+cat $1 | grep -Fv -e "/tests" > build/coverage/cover.out
 
 go tool cover -func=build/coverage/cover.out | grep total:
 # Generate coverage report in html format
 go tool cover -html=build/coverage/cover.out -o $2
 
 # Remove temporary file
-rm $1 build/coverage/cover.out || true
+rm build/coverage/cover.out || true
